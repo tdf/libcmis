fcac389d85c781491a22f7f8fb352e7df5bc1365
Mihai Varga <mihai.mv13@gmail.com>
2014-06-16 17:54:46 +0300

    OneDrive Document class

---
+++ src/libcmis/onedrive-document.cxx
@@ -0,0 +1,189 @@
+/* libcmis
+ * Version: MPL 1.1 / GPLv2+ / LGPLv2+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * Copyright (C) 2014 Mihai Varga <mihai.mv13@gmail.com>
+ *
+ *
+ * All Rights Reserved.
+ *
+ * For minor contributions see the git repository.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPLv2+"), or
+ * the GNU Lesser General Public License Version 2 or later (the "LGPLv2+"),
+ * in which case the provisions of the GPLv2+ or the LGPLv2+ are applicable
+ * instead of those above.
+ */
+
+#include "onedrive-document.hxx"
+#include "onedrive-folder.hxx"
+#include "onedrive-session.hxx"
+#include "onedrive-utils.hxx"
+#include "json-utils.hxx"
+#include "rendition.hxx"
+
+using namespace std;
+using namespace libcmis;
+
+OneDriveDocument::OneDriveDocument( OneDriveSession* session ) :
+    libcmis::Object( session),
+    libcmis::Document( session ),
+    OneDriveObject( session )
+{
+}
+
+OneDriveDocument::OneDriveDocument( OneDriveSession* session, Json json, string id, string name ) :
+    libcmis::Object( session),
+    libcmis::Document( session ),
+    OneDriveObject( session, json, id, name )
+{
+}
+
+OneDriveDocument::~OneDriveDocument( )
+{
+}
+
+vector< libcmis::FolderPtr > OneDriveDocument::getParents( ) 
+    throw ( libcmis::Exception )
+{
+    vector< libcmis::FolderPtr > parents;
+
+    string parentId = getStringProperty( "cmis:parentId" );
+   
+    libcmis::ObjectPtr obj = getSession( )->getObject( parentId );
+    libcmis::FolderPtr parent = boost::dynamic_pointer_cast< libcmis::Folder >( obj );
+    parents.push_back( parent );
+    return parents;
+}
+
+boost::shared_ptr< istream > OneDriveDocument::getContentStream( string /*streamId*/ )
+    throw ( libcmis::Exception )
+{
+    boost::shared_ptr< istream > stream;
+    string streamUrl = getStringProperty( "source" );
+    if ( streamUrl.empty( ) )
+        throw libcmis::Exception( "can not found stream url" );
+
+    try
+    {
+        stream = getSession( )->httpGetRequest( streamUrl )->getStream( );
+    }
+    catch ( const CurlException& e )
+    {
+        throw e.getCmisException( );
+    }
+    return stream;
+}
+
+void OneDriveDocument::uploadStream( boost::shared_ptr< ostream > os, 
+                                   string contentType )
+                                throw ( libcmis::Exception )
+{
+    if ( !os.get( ) )
+        throw libcmis::Exception( "Missing stream" );
+
+    string putUrl = getStringProperty( "upload_location" );
+    
+    // Upload stream
+    boost::shared_ptr< istream> is ( new istream ( os->rdbuf( ) ) );
+    vector <string> headers;
+    headers.push_back( string( "Content-Type: " ) + contentType );
+    try
+    {
+        getSession()->httpPutRequest( putUrl, *is, headers );
+    }
+    catch ( const CurlException& e )
+    {
+        throw e.getCmisException( );
+    }
+    long httpStatus = getSession( )->getHttpStatus( );
+    if ( httpStatus < 200 || httpStatus >= 300 )
+        throw libcmis::Exception( "Document content wasn't set for"
+                "some reason" );
+    refresh( );
+}
+
+void OneDriveDocument::setContentStream( boost::shared_ptr< ostream > os, 
+                                       string contentType, 
+                                       string fileName, 
+                                       bool /*overwrite*/ ) 
+                                            throw ( libcmis::Exception )
+{
+    if ( !os.get( ) )
+        throw libcmis::Exception( "Missing stream" );
+    
+    string metaUrl = getUrl( );
+
+    // Update file name meta information
+    if ( !fileName.empty( ) && fileName != getContentFilename( ) )
+    {
+        Json metaJson;
+        Json fileJson( fileName.c_str( ) );
+        metaJson.add("name", fileJson );
+
+        std::istringstream is( metaJson.toString( ) );
+        vector<string> headers;
+        headers.push_back( "Content-Type: application/json" );
+        try
+        {
+            getSession()->httpPutRequest( metaUrl, is, headers );
+        }
+        catch ( const CurlException& e )
+        {
+            throw e.getCmisException( );
+        }
+    }
+
+    // Upload stream
+    uploadStream( os, contentType );
+}
+
+libcmis::DocumentPtr OneDriveDocument::checkOut( ) throw ( libcmis::Exception )
+{
+    // OneDrive doesn't have CheckOut, so just return the same document here
+    libcmis::ObjectPtr obj = getSession( )->getObject( getId( ) );
+    libcmis::DocumentPtr checkout =
+        boost::dynamic_pointer_cast< libcmis::Document > ( obj );
+    return checkout;
+}
+
+void OneDriveDocument::cancelCheckout( ) throw ( libcmis::Exception )
+{
+    // Don't do anything since we don't have CheckOut
+}
+
+libcmis::DocumentPtr OneDriveDocument::checkIn( 
+    bool /*isMajor*/, 
+    std::string /*comment*/,
+    const PropertyPtrMap& properties,
+    boost::shared_ptr< std::ostream > stream,
+    std::string contentType, 
+    std::string fileName ) 
+        throw ( libcmis::Exception )
+{     
+    // OneDrive doesn't have CheckIn, so just upload the properties, 
+    // the content stream and fetch the new document resource.
+    updateProperties( properties );
+    setContentStream( stream, contentType, fileName );
+    libcmis::ObjectPtr obj = getSession( )->getObject( getId( ) );
+    libcmis::DocumentPtr checkin = 
+        boost::dynamic_pointer_cast< libcmis::Document > ( obj );
+    return checkin;
+}
+
+vector< libcmis::DocumentPtr > OneDriveDocument::getAllVersions( ) 
+    throw ( libcmis::Exception )
+{   
+    return vector< libcmis::DocumentPtr > ( );
+}
---
+++ src/libcmis/onedrive-document.hxx
@@ -0,0 +1,82 @@
+/* libcmis
+ * Version: MPL 1.1 / GPLv2+ / LGPLv2+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * Copyright (C) 2014 Mihai Varga <mihai.mv13@gmail.com>
+ *
+ *
+ * All Rights Reserved.
+ *
+ * For minor contributions see the git repository.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPLv2+"), or
+ * the GNU Lesser General Public License Version 2 or later (the "LGPLv2+"),
+ * in which case the provisions of the GPLv2+ or the LGPLv2+ are applicable
+ * instead of those above.
+ */
+
+#ifndef _ONEDRIVE_DOCUMENT_HXX_
+#define _ONEDRIVE_DOCUMENT_HXX_
+
+#include "document.hxx"
+#include "folder.hxx"
+#include "onedrive-object.hxx"
+#include "json-utils.hxx"
+#include "rendition.hxx"
+
+class OneDriveDocument : public libcmis::Document, public OneDriveObject
+{
+    public:
+        OneDriveDocument( OneDriveSession* session );
+
+        OneDriveDocument( OneDriveSession* session, Json json, 
+                        std::string id = std::string( ),
+                        std::string name = std::string( ) );
+        ~OneDriveDocument( );
+        
+        std::string getType( ) { return std::string( "cmis:document" );}
+        std::string getBaseType( ) { return std::string( "cmis:document" );} 
+
+        void uploadStream( boost::shared_ptr< std::ostream > os, 
+                                       std::string contentType )
+                                             throw ( libcmis::Exception );
+
+        virtual std::vector< libcmis::FolderPtr > getParents( ) 
+                    throw (libcmis::Exception );
+        virtual boost::shared_ptr< std::istream > getContentStream( 
+                std::string streamId = std::string( ) ) throw (libcmis::Exception );
+        
+        virtual void setContentStream( boost::shared_ptr< std::ostream > os, 
+                                       std::string contentType,
+                                       std::string fileName, 
+                                       bool overwrite = true ) 
+                                           throw ( libcmis::Exception );
+
+        virtual libcmis::DocumentPtr checkOut( ) throw ( libcmis::Exception );
+        virtual void cancelCheckout( ) throw ( libcmis::Exception );
+        virtual libcmis::DocumentPtr checkIn( 
+                    bool isMajor, 
+                    std::string comment,
+                    const std::map< std::string,libcmis::PropertyPtr >& 
+                        properties,
+                    boost::shared_ptr< std::ostream > stream,
+                    std::string contentType, 
+                    std::string fileName ) 
+                        throw ( libcmis::Exception );
+        
+        virtual std::vector< libcmis::DocumentPtr > getAllVersions( )
+            throw (libcmis::Exception );
+};
+
+#endif
