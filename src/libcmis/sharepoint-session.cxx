ed9880d712ad6dacc117b637c31244038190ebd2
Mihai Varga <mihai.mv13@gmail.com>
2014-07-07 15:16:46 +0300

    SharePoint session class

---
+++ src/libcmis/sharepoint-session.cxx
@@ -0,0 +1,107 @@
+/* libcmis
+ * Version: MPL 1.1 / GPLv2+ / LGPLv2+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * Copyright (C) 2014 Mihai Varga <mihai.mv13@gmail.com>
+ *
+ *
+ * All Rights Reserved.
+ *
+ * For minor contributions see the git repository.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPLv2+"), or
+ * the GNU Lesser General Public License Version 2 or later (the "LGPLv2+"),
+ * in which case the provisions of the GPLv2+ or the LGPLv2+ are applicable
+ * instead of those above.
+ */
+
+#include "sharepoint-repository.hxx"
+#include "sharepoint-session.hxx"
+
+using namespace std;
+
+SharePointSession::SharePointSession ( string baseUrl,
+                               string username,
+                               string password,
+                               bool verbose )
+                                    throw ( libcmis::Exception ) :
+    BaseSession( baseUrl, string(), username, password, false,
+                 libcmis::OAuth2DataPtr(), verbose )
+
+{
+    setNTLMAuthentication( true );
+
+    libcmis::HttpResponsePtr response;
+    try
+    {
+        response = httpGetRequest( baseUrl + "/currentuser" );
+    }
+    catch ( const CurlException& e )
+    {
+        // It's not SharePoint or wrong username/passwd provided
+        throw e.getCmisException( );
+    }
+
+    // Add the dummy repository
+    m_repositories.push_back( getRepository( ) );
+}
+
+SharePointSession::SharePointSession( const SharePointSession& copy ) :
+    BaseSession( copy )
+{
+}
+
+SharePointSession::SharePointSession() :
+    BaseSession()
+{
+}
+
+SharePointSession::~SharePointSession()
+{
+}
+
+libcmis::RepositoryPtr SharePointSession::getRepository( )
+    throw ( libcmis::Exception )
+{
+    // Return a dummy repository since SharePoint doesn't have that notion
+    libcmis::RepositoryPtr repo( new SharePointRepository( ) );
+    return repo;
+}
+
+libcmis::ObjectPtr SharePointSession::getObject( string /*objectId*/ )
+    throw ( libcmis::Exception )
+{
+    libcmis::ObjectPtr object;
+    return object;
+}
+
+libcmis::ObjectPtr SharePointSession::getObjectByPath( string path )
+    throw ( libcmis::Exception )
+{
+    return getObject( path );
+}
+
+libcmis::ObjectTypePtr SharePointSession::getType( string /*id*/ )
+    throw ( libcmis::Exception )
+{
+    libcmis::ObjectTypePtr type;
+    return type;
+}
+
+vector< libcmis::ObjectTypePtr > SharePointSession::getBaseTypes( )
+    throw ( libcmis::Exception )
+{
+    vector< libcmis::ObjectTypePtr > types;
+    return types;
+}
---
+++ src/libcmis/sharepoint-session.hxx
@@ -0,0 +1,69 @@
+/* libcmis
+ * Version: MPL 1.1 / GPLv2+ / LGPLv2+
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License or as specified alternatively below. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Major Contributor(s):
+ * Copyright (C) 2014 Mihai Varga <mihai.mv13@gmail.com>
+ *
+ *
+ * All Rights Reserved.
+ *
+ * For minor contributions see the git repository.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPLv2+"), or
+ * the GNU Lesser General Public License Version 2 or later (the "LGPLv2+"),
+ * in which case the provisions of the GPLv2+ or the LGPLv2+ are applicable
+ * instead of those above.
+ */
+#ifndef _SHAREPOINT_SESSION_HXX_
+#define _SHAREPOINT_SESSION_HXX_
+
+#include "base-session.hxx"
+#include "repository.hxx"
+#include "json-utils.hxx"
+
+class SharePointSession : public BaseSession
+{
+    public:
+        SharePointSession( std::string baseUrl,
+                       std::string username, 
+                       std::string password,
+                       bool verbose = false )
+                   throw ( libcmis::Exception );
+
+        SharePointSession( const SharePointSession& copy );
+
+        ~SharePointSession ( );
+
+        virtual libcmis::RepositoryPtr getRepository( ) 
+            throw ( libcmis::Exception );
+
+        virtual bool setRepository( std::string ) { return true; }
+
+        virtual libcmis::ObjectPtr getObject( std::string id ) 
+            throw ( libcmis::Exception );
+
+        virtual libcmis::ObjectPtr getObjectByPath( std::string path ) 
+            throw ( libcmis::Exception );
+
+        virtual libcmis::ObjectTypePtr getType( std::string id )             
+            throw ( libcmis::Exception );
+        
+        virtual std::vector< libcmis::ObjectTypePtr > getBaseTypes( )
+            throw ( libcmis::Exception );
+
+    private:
+        SharePointSession( );
+};
+
+#endif /* _SHAREPONT_SESSION_HXX_ */
